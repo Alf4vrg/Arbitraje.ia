de5a6263185e206f86e3da418ac1c3a22e480fa8
Alf4vrg <83315552+Alf4vrg@users.noreply.github.com>
2026-03-29 17:58:45 +0000

    fix aliexpress source import and funtion

--- sources/aliexpress.py
+++ sources/aliexpress.py
@@ -1,21 +1,25 @@
+from core.models import Product
 from config.settings import KEYWORDS
 
-products = []
 
-for keyword in KEYWORDS:
-    query = keyword.replace(" ", "+")
+def obtener_productos_aliexpress():
+    products = []
 
-    products.append(
-        Product(
-            source="aliexpress",
-            title=keyword,
-            category="smartphones",
-            price=150.0,  # dummy por ahora
-            currency="USD",
-            rating=4.5,
-            discount_percent=10.0,
-            image_url="https://via.placeholder.com/150",
-            
-product_url=f"https://www.aliexpress.com/wholesale?SearchText={query}",
+    for keyword in KEYWORDS:
+        query = keyword.replace(" ", "+")
+
+        products.append(
+            Product(
+                source="aliexpress",
+                title=keyword,
+                category="smartphones",
+                price=150.0,
+                currency="USD",
+                rating=4.5,
+                discount_percent=10.0,
+                image_url="https://via.placeholder.com/150",
+                product_url=f"https://www.aliexpress.com/wholesale?SearchText={query}",
+            )
         )
-    )
+
+    return products
