499593e17cb11ebefc0769af7b28b2bad2f474d3
Alf4vrg <83315552+Alf4vrg@users.noreply.github.com>
2026-03-29 19:36:15 +0000

    3

--- sources/manual.py
+++ sources/manual.py
@@ -5,8 +5,19 @@ def obtener_productos_manual():
     return [
     Product(
         source="manual",
-        title="Mini impresora térmica HPRT bluetooth MT53",
+        title="smartwach deportivos",
         category="mobile-accessories",
+        price=119.81,
+        currency="MXN",
+        rating=4.2,
+        discount_percent=0.0,
+        image_url="https://ae01.alicdn.com/kf/placeholder2.jpg",
+        product_url="https://es.aliexpress.com/item/1005009727258458.html?spm=a2g0o.productlist.main.2.3afc6ePm6ePmlW&algo_pvid=cf1733c9-763d-4c0a-9104-6c2249127b64&algo_exp_id=cf1733c9-763d-4c0a-9104-6c2249127b64-1&pdp_ext_f=%7B%22order%22%3A%22-1%22%2C%22eval%22%3A%221%22%2C%22fromPage%22%3A%22search%22%7D&pdp_npi=6%40dis%21MXN%21174.58%21119.81%21%21%2166.02%2145.31%21%4021030a4b17748128699602764e117f%2112000049963449234%21sea%21MX%210%21ABX%211%210%21n_tag%3A-29910%3Bd%3Abc4d4f2f%3Bm03_new_user%3A-29895%3BpisId%3A5000000197832000&curPageLogUid=WKa7Lm97rcmD&utparam-url=scene%3Asearch%7Cquery_from%3A%7Cx_object_id%3A1005009727258458%7C_p_origin_prod%3A",
+    ),
+    Product(
+        source="manual",
+        title="audifonos bluetooth gaming",
+        category="gaming-accessories",
         price=273.26,
         currency="MXN",
         rating=4.9,
@@ -15,5 +26,15 @@ def obtener_productos_manual():
         product_url="https://es.aliexpress.com/item/1005010653195874.html?spm=a2g0o.productlist.main.7.4f6a8yjX8yjXsj&algo_pvid=67462acb-6449-48a4-bb80-7851b9f939e6&algo_exp_id=67462acb-6449-48a4-bb80-7851b9f939e6-6&pdp_ext_f=%7B%22order%22%3A%22-1%22%2C%22eval%22%3A%221%22%2C%22orig_sl_item_id%22%3A%221005010653195874%22%2C%22orig_item_id%22%3A%221005008261580807%22%2C%2２fromPage%２%3A%２search％7D&pdp_npi=6%40dis%21MXN%21546.89%21273.26%21%21%21206.82%21103.34%21%4021030a4b17748117575338750e1187%2112000053087679688%21sea%21MX%210%21ABX%211%210%21n_tag%3A-29910%3Bd%3Abc4d4f2f%3Bm03_new_user%3A-29895%3BpisId%3A5000000200838266&curPageLogUid=TTGnf9kLzGUM&utparam-url=scene%3Asearch%7Cquery_from%3A%7Cx_object_id%3A1005010653195874%7C_p_origin_prod%3A1005008261580807",
 
     ),
-
+    Product(
+        source="manual",
+        title="Mini impresora térmica HPRT bluetooth MT53",
+        category="mobile-accessories",
+        price=152.63,
+        currency="MXN",
+        rating=3.9,
+        discount_percent=0.0,
+        image_url="https://ae01.alicdn.com/kf/placeholder2.jpg",
+        product_url="https://es.aliexpress.com/item/1005011595270902.html?spm=a2g0o.productlist.main.5.e2977c1d7cKm1k&algo_pvid=4a3d9f3c-24ea-4047-ae7c-af0c1c478d98&algo_exp_id=4a3d9f3c-24ea-4047-ae7c-af0c1c478d98-4&pdp_ext_f=%7B%22order%22%3A%22-1%22%2C%22spu_best_type%22%3A%22price%22%2C%22eval%22%3A%221%22%2C%22fromPage%22%3A%22search%22%7D&pdp_npi=6%40dis%21MXN%21220.40%21165.63%21%21%2183.35%2162.64%21%402101e8f317748125835816370e3cea%2112000056013531477%21sea%21MX%210%21ABX%211%210%21n_tag%3A-29910%3Bd%3Abc4d4f2f%3Bm03_new_user%3A-29895%3BpisId%3A5000000197832000&curPageLogUid=peqXtuqSFz15&utparam-url=scene%3Asearch%7Cquery_from%3A%7Cx_object_id%3A1005011595270902%7C_p_origin_prod%3A",
+    ),
 ]
