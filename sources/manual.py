1a2543eda2dd8e9ff7513a8a3b7935f0a55160db
Alf4vrg <83315552+Alf4vrg@users.noreply.github.com>
2026-03-29 19:38:55 +0000

    3

--- sources/manual.py
+++ sources/manual.py
@@ -24,7 +24,6 @@ def obtener_productos_manual():
         discount_percent=0.0,
         image_url="https://ae01.alicdn.com/kf/placeholder2.jpg",
         product_url="https://es.aliexpress.com/item/1005010653195874.html?spm=a2g0o.productlist.main.7.4f6a8yjX8yjXsj&algo_pvid=67462acb-6449-48a4-bb80-7851b9f939e6&algo_exp_id=67462acb-6449-48a4-bb80-7851b9f939e6-6&pdp_ext_f=%7B%22order%22%3A%22-1%22%2C%22eval%22%3A%221%22%2C%22orig_sl_item_id%22%3A%221005010653195874%22%2C%22orig_item_id%22%3A%221005008261580807%22%2C%2２fromPage%２%3A%２search％7D&pdp_npi=6%40dis%21MXN%21546.89%21273.26%21%21%21206.82%21103.34%21%4021030a4b17748117575338750e1187%2112000053087679688%21sea%21MX%210%21ABX%211%210%21n_tag%3A-29910%3Bd%3Abc4d4f2f%3Bm03_new_user%3A-29895%3BpisId%3A5000000200838266&curPageLogUid=TTGnf9kLzGUM&utparam-url=scene%3Asearch%7Cquery_from%3A%7Cx_object_id%3A1005010653195874%7C_p_origin_prod%3A1005008261580807",
-
     ),
     Product(
         source="manual",
