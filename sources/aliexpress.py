72ae724e8c75edbfecc94570212d947784e2a6a7
Alf4vrg <83315552+Alf4vrg@users.noreply.github.com>
2026-03-29 17:04:29 +0000

    pruea 1

--- sources/aliexpress.py
+++ sources/aliexpress.py
@@ -1,3 +1,28 @@
+from core.models import Product
+
+
 def obtener_productos_aliexpress():
-    # aquí luego conectamos scraping/API
-    return []
+    return [
+        Product(
+            source="aliexpress",
+            title="Auriculares Bluetooth TWS",
+            category="mobile-accessories",
+            price=12.5,
+            currency="USD",
+            rating=4.7,
+            discount_percent=18.0,
+            image_url="https://example.com/image1.jpg",
+            product_url="https://example.com/producto1"
+        ),
+        Product(
+            source="aliexpress",
+            title="Smartphone Android 8GB 256GB",
+            category="smartphones",
+            price=145.0,
+            currency="USD",
+            rating=4.5,
+            discount_percent=12.0,
+            image_url="https://example.com/image2.jpg",
+            product_url="https://example.com/producto2"
+        ),
+    ]
